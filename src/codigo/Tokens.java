149e2e59f18f4df3fcb218d983763c7a194344b4
Vicente <vicentemartinezguzman15@gmail.com>
2026-04-20 11:54:30 -0600

    Primer commit

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author Charly Ponce
+ */
+
+public enum Tokens {
+    Linea,
+    Comillas,
+    Reservado,
+    Inter,
+    Cadena,
+    If,
+    Else,
+    Do,
+    While,
+    For,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Operador_logico,
+    Operador_incremento,
+    Operador_relacional,
+    Operador_atribucion,
+    Operador_booleano,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    Main,
+    Finalizador,
+    Identificador,
+    Numero,
+    ERROR
+}
