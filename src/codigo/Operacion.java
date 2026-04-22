296f335ae4cd1c8bd690070bbbc63b42e029283a
Vicente <vicentemartinezguzman15@gmail.com>
2026-04-22 14:19:28 -0600

    No se hizo ningún cambio

--- src/codigo/Operacion.java
+++ src/codigo/Operacion.java
@@ -4,10 +4,15 @@
  */
 package codigo;
 
+import java.io.IOException;
+
 /**
  *
  * @author vicen
  */
 public abstract class Operacion {
-    
+    public abstract void sumar() throws Exception;
+    public abstract void restar() throws Exception;
+    public abstract void multiplicar() throws Exception;
+    public abstract void dividir() throws Exception;
 }
